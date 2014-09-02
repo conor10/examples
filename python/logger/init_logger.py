0c11442dc480e20d6177a0c683116006de8019f1
Conor <conor10@gmail.com>
2014-09-02 21:31:53 +1000

    Added logging example

---
+++ python/logger/init_logger.py
@@ -0,0 +1,20 @@
+import logging.config
+import os
+import yaml
+
+
+def setup(default_path='config/error_logging.yaml',
+          default_level=logging.INFO,
+          env_key='LOG_CFG'):
+
+    path = default_path
+    value = os.getenv(env_key, None)
+    if value:
+        path = value
+    if os.path.exists(path):
+        with open(path, 'rt') as f:
+            config = yaml.load(f.read())
+            logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=default_level)
+
---
+++ python/logger/main.py
@@ -0,0 +1,13 @@
+import logging
+
+import init_logger
+
+def log_an_error():
+    log = logging.getLogger(__name__)
+    log.error("Uh oh, something's not right")
+
+
+if __name__ == '__main__':
+    init_logger.setup()
+    logging.info("Wow, that was simple...")
+    log_an_error()
