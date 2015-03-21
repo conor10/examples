ed7fd4a6fec1a479943cdb194412ba703e5dd033
Conor <conor10@gmail.com>
2015-03-21 18:02:56 +1100

    Fixed broken test.

--- python/drawdown/test/test_utils.py
+++ python/drawdown/test/test_utils.py
@@ -18,9 +18,10 @@ class TestUtils(unittest.TestCase):
         returns = np.array(
             [0.1, 0.2, -0.1, -0.2, 0.1, -0.2, 0.1, 0.1, 0.1, 0.1, 0.3])
         compound_returns = (1. + returns).cumprod() - 1.
-        max_dd, max_duration, dd_offset, duration_offset = \
+        max_dd, max_duration, dd_idx, duration_idx, hwm_idx = \
             utils.calculate_max_drawdown(compound_returns)
         self.assertAlmostEqual(-0.3664, max_dd)
         self.assertEqual(8, max_duration)
-        self.assertEqual(5, dd_offset)
-        self.assertEqual(9, duration_offset)
+        self.assertEqual(5, dd_idx)
+        self.assertEqual(9, duration_idx)
+        self.assertEqual(1, hwm_idx)
